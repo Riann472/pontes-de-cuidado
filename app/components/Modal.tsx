e93fde2258b005f97b86c37f5179fc2be7fb153e
Riann472 <rianncarvalhomota472@gmail.com>
2025-10-29 12:40:55 -0300

    fix: modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,17 +1,17 @@
-import images from "app\images\organs\caps.png"
+import caps from "../images/CRAS_teste.png"
+import Image from "next/image"
 
 const Modal = () => {
   return (
-
     <div className="h-screen w-screen bg-[#000000bb] fixed z-20 flex items-center justify-center">
       <div className="bg-white py-15 pl-10 w-200 h-200 border rounded-xl flex flex-wrap gap-13">
         <h1 className="text-5xl ">
           CRAS
         </h1>
         <Image
-          src={cras}
+          src={caps}
           alt="cras"
-          className="w-full h-full object-cover object-center"
+          width={350}
         />
         <p className="main-text">
           O Centro de Referência de Assistência Social do Brasil é uma unidade responsável pela oferta de serviços de proteção básica do Sistema Único de Assistência Social, nas áreas de vulnerabilidade e risco social.
