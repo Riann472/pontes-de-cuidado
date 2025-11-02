da1a3caf6cb239e4fe6ba5d2d023667a3a8eec8a
Riann <rianncarvalhomota472@gmail.com>
2025-11-02 18:00:11 -0300

    feat: open the modal with the correct data

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,39 +1,57 @@
+import { Dispatch, SetStateAction } from "react"
+import { organ } from "../data/organs"
+import Image from "next/image"
 
-const Modal = () => {
+interface ModalProps {
+  setOpenModal: Dispatch<SetStateAction<boolean>>
+  modalData: organ
+}
+
+
+const Modal = ({ setOpenModal, modalData }: ModalProps) => {
   return (
+    <div className="fixed inset-0 z-20 bg-[#000000bb] flex items-center justify-center">
+      <div className="z-30 bg-white w-[85vw] h-[85vh] border rounded-4xl flex flex-col overflow-auto p-8">
+        <div className="flex justify-between items-start">
+          <h1 className="text-xl cursor-pointer" onClick={() => setOpenModal(false)}>X</h1>
+        </div>
 
-    <div className="h-screen w-screen bg-[#000000bb] fixed z-20 flex  items-center justify-center ">
-      <div className="bg-white py-15 pl-10 w-350 h-200 border rounded-4xl flex flex-wrap gap-8">
-        <img className="w-125 m-5"
-          src="https://santaritadoaraguaia.go.gov.br/wp-content/uploads/2021/05/CRAS.png">
-        </img>
-        <div className="flex flex-wrap gap-4 flex-1 justify-center">
-          <h1 className="text-5xl font-bold w-150">Centro de Referência de Assistência Social (CRAS)</h1>
-          <p className="text-3xl">
-            O Centro de Referência de Assistência Social do Brasil é uma unidade responsável pela oferta de serviços de proteção básica do Sistema Único de Assistência Social, nas áreas de vulnerabilidade e risco social.
-          </p>
+        <div className="flex gap-20 mt-4">
+          <div className="w-[40%]">
+            <Image src={modalData.image} alt={modalData.name} />
+          </div>
+          <div className="flex flex-col gap-5 flex-1">
+            <h1 className="text-3xl font-bold">
+              {modalData.name} {modalData.fullname.length >= 1 && `(${modalData.fullname})`}
+            </h1>
+            <p className="text-xl text-justify">{modalData.description}</p>
+          </div>
         </div>
-        {/* Informações */}
-        <div className="flex flex-wrap py-10 gap-20">
-          <ul className="contato text-2xl">
-            <h2 className="font-bold text-3xl">Informações de contato</h2>
-            <li>(85) 99999-9999</li>
-            <li>(85) 99999-9999</li>
-          </ul>
-          <ul className="text-2xl">
-            <h2 className="font-bold text-3xl">Endereços</h2>
-            <li>Rua das Laranjeiras, 25</li>
-            <li>Rua das Laranjeiras, 25</li>
-          </ul>
-        </div>
-        {/* Botão */}
-        <div className="ml-50 flex items-center justify-center">
-          <button className="text-white bg-primary w-35 h-30 border rounded-2xl">
-            <a href="#">Ver no mapa</a>
-          </button>
+
+        <div className="mt-10">
+          <h2 className="font-bold text-2xl text-center mb-1">Informações de contato e endereços</h2>
+          <h4 className="font-semibold text-base text-center mb-6">Clique nos botoes de endereço para ir ao mapa!</h4>
+          <div className="flex flex-wrap gap-8 justify-center">
+            {modalData.organUnitys?.length == 0 && (
+              <p>Dados indisponiveis.</p>
+            )}
+            {modalData.organUnitys?.map((unity, idx) => (
+              <div key={idx} className="flex flex-col items-center gap-4">
+                <h1 className="font-semibold text-xl">{unity.district}</h1>
+                <button className="text-black font-bold bg-primary px-3 py-5 rounded-2xl cursor-pointer">
+                  <p>{unity.address}</p>
+                </button>
+                <div className="w-full flex flex-col items-center gap-1">
+                  <p><span className="font-semibold">Número do coordenador:</span> {unity.coordNumber}</p>
+                  <p><span className="font-semibold">Número do orgão:</span> {unity.organNumber}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div >
+    </div>
   )
 }
+
 export default Modal
--- app/data/organs.ts
+++ app/data/organs.ts
@@ -19,8 +19,9 @@ export interface organUnity {
 export interface organ {
     id: number
     name: string
+    fullname: string
     description: string
-    organUnitys?: organUnity[]
+    organUnitys: organUnity[]
     image: StaticImageData
 }
 
@@ -28,49 +29,87 @@ const organs: organ[] = [
     {
         id: 1,
         name: "CRAS",
-        description: "Atende famílias em situação de vulnerabilidade social, oferecendo apoio, orientações, acesso a benefícios e atividades que fortalecem os vínculos familiares e comunitários.",
+        description: "O CRAS (Centro de Referência de Assistência Social) é um serviço público que atende famílias em situação de vulnerabilidade social. Oferece apoio, orientação e acompanhamento para acesso a direitos e benefícios sociais. Promove atividades que fortalecem os vínculos familiares e comunitários. Atua na prevenção de situações de risco social e na inclusão das famílias em programas de proteção e promoção social. É um ponto de referência para orientação e suporte à comunidade local.",
+        fullname: "Centro de Referência de Assistência Social",
+        organUnitys: [
+            {
+                address: "Av. Sabino Monte, 4506 - São João do Tauape",
+                coordNumber: "(85) 99999-9999",
+                organNumber: "(85) 98888-8888",
+                district: "Lagamar",
+                mapLink: "https://www.google.com/maps"
+            },
+            {
+                address: "Av. João Pessoa, 4474 - Damas",
+                coordNumber: "(85) 99999-9999",
+                organNumber: "(85) 98888-8888",
+                district: "Couto Fernandes",
+                mapLink: "https://www.google.com/maps"
+            },
+            {
+                address: "R. Do Avião, s/n - Vila União",
+                coordNumber: "(85) 99999-9999",
+                organNumber: "(85) 98888-8888",
+                district: "Vila União",
+                mapLink: "https://www.google.com/maps"
+            }
+        ],
         image: Cras
     },
     {
         id: 2,
         name: "CREAS",
         description: "Atende pessoas e famílias que vivenciam situações de violação de direitos, como violência doméstica, abuso, abandono ou trabalho infantil, oferecendo acompanhamento.",
+        fullname: "Centro de Referência Especializado de Assistência Social",
+        organUnitys: [],
         image: Creas
     },
     {
         id: 3,
         name: "CAPS",
         description: "Oferece atendimento gratuito a pessoas com sofrimento mental ou uso abusivo de álcool e outras drogas, com apoio psicológico, psiquiátrico e social.",
+        fullname: "Centro de Atenção Psicossocial",
+        organUnitys: [],
         image: Caps
     },
     {
         id: 4,
         name: "UBS",
         description: "É a porta de entrada do SUS, onde são realizados atendimentos médicos, vacinação, exames básicos e acompanhamento de gestantes, crianças e idosos.",
+        fullname: "Unidades Básicas de Saúde",
+        organUnitys: [],
         image: Ubs
     },
     {
         id: 5,
-        name: "CLÍNICAS ESCOLA",
+        name: "Clínicas Escola",
         description: "Vinculadas a faculdades, oferecem atendimentos gratuitos ou de baixo custo em áreas como psicologia, fisioterapia, nutrição e outras, com supervisão de professores e profissionais.",
+        fullname: "",
+        organUnitys: [],
         image: Escola
     },
     {
         id: 6,
-        name: "CENTROS LGBTQIA+",
+        name: "Centros LGBTIQA+",
         description: "Oferecem acolhimento, escuta, orientação jurídica, psicológica e social para pessoas LGBTQIA+, além de promoverem ações de combate à discriminação e inclusão.",
+        fullname: "",
+        organUnitys: [],
         image: Lgbt
     },
     {
         id: 7,
-        name: "CENTROS POP",
+        name: "Centros POP",
         description: "Atendem pessoas em situação de rua, oferecendo alimentação, higiene, guarda de pertences, encaminhamentos e apoio para reinserção social.",
+        fullname: "",
+        organUnitys: [],
         image: Pop
     },
     {
         id: 8,
         name: "ONG'S",
         description: "Instituições sem fins lucrativos que atuam em diversas causas sociais e prestam serviços à comunidade, como realização de doações, proteção animal, educação, entre outros auxílios.",
+        fullname: "Organizações Não Governamentais",
+        organUnitys: [],
         image: Ong
     },
 ]
